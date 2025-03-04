27b7d76e63b223a21d7c1ca3deaa2956ced5213d
Joseph Lee <chihlee10009@gmail.com>
2025-03-04 15:20:34 -0500

    uploading my morse code srcipt from my mac

---
+++ pyscribble02272025_morse_code.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+#python Dictionary
+morse_code = {
+"a" : ".-",
+"b"	: "-...",
+"c"	: "-.-.",
+"d" : "-..",
+"e"	: ".",
+"f"	: "..-.",
+"g"	: "--.",
+"h"	: "....",
+"i"	: "..",
+"j"	: ".---",
+"k"	: "-.-",
+"l"	: ".-..",
+"m"	: "--",
+"n"	: "-.",
+"o"	: "---",
+"p"	: ".--.",
+"q"	: "--.-",
+"r"	: ".-.",
+"s"	: "...",
+"t"	: "-",
+"u"	: "..-",
+"v"	: "...-",
+"w"	: ".--",
+"x"	: "-..-",
+"y"	: "-.--",
+"z"	: "--.."
+}  
+
+msg = "helpme"
+enc_msg = ""
+
+for letter in msg:
+    enc_msg += morse_code[letter] + " "
+    for sound in morse_code[letter]:
+        if sound == ".":
+            os.system("say da")
+        elif sound == "-":
+            os.system("say dash")
+    time.sleep(1)
+print(enc_msg)
